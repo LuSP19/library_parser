bcbf316b284bb790072bfff2c68db309eca61a24
LuSP19 <semyon.starykh@gmail.com>
2022-04-14 08:45:10 +0500

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from pathlib import Path
+
 import requests
 
 
-url = 'https://tululu.org/txt.php?id=32168'
+Path('books').mkdir(exist_ok=True)
 
-response = requests.get(url)
-response.raise_for_status()
-with open('Mars Sands.txt', 'wb') as file:
-    file.write(response.content)
+url_pattern = 'https://tululu.org/txt.php?id={0}'
+
+for book_id in range(1,11):
+    url = url_pattern.format(book_id)
+    response = requests.get(url)
+    response.raise_for_status()
+
+    filepath = Path('books', f'{book_id}.txt')
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
